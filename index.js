205a8f3adbcaaa9e663e71e4edc04bd595802582
RBenOthmen <ramibenothmen15@gmail.com>
2024-10-10 16:45:59 +0100

    Added the prerequisites for the project and created the validateMovie function

---
+++ index.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const movies = [
+    { id: 1, name: "Movie_1" },
+    { id: 2, name: "Movie_2" },
+    { id: 3, name: "Movie_3" },
+];
+
+
+// -------------------------------------------------- VALIDATE --------------------------------------------------
+function validateMovie(movie) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+    });
+    return schema.validate(movie);
+}
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
