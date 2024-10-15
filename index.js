4a12799e2513c90a054282ddfabab92b80945350
RBenOthmen <ramibenothmen15@gmail.com>
2024-10-15 15:13:46 +0100

    Project- Add Persistence to Genres API

--- index.js
+++ index.js
@@ -1,77 +1,14 @@
-const Joi = require("joi");
-const express = require("express");
+const mongoose = require('mongoose');
+const genres = require('./routes/genres');
+const express = require('express');
 const app = express();
 
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
 app.use(express.json());
-
-const movies = [
-    { id: 1, name: "Movie_1" },
-    { id: 2, name: "Movie_2" },
-    { id: 3, name: "Movie_3" },
-];
-
-// -------------------------------------------------- GET HOME --------------------------------------------------
-app.get("/", (req, res) => {
-    res.send("Welcome to VIDLY");
-});
-
-// -------------------------------------------------- GET ALL --------------------------------------------------
-app.get("/api/movies", (req, res) => {
-    res.send(movies);
-});
-
-// -------------------------------------------------- GET by ID --------------------------------------------------
-app.get("/api/movies/:id", (req, res) => {
-    const movie = movies.find((c) => c.id === parseInt(req.params.id));
-    if (!movie) return res.status(404).send("The movie with the given ID was not found.");
-    res.send(movie);
-});
-
-// -------------------------------------------------- POST --------------------------------------------------
-app.post("/api/movies", (req, res) => {
-    const { error } = validateMovie(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const movie = {
-        id: movies.length + 1,
-        name: req.body.name,
-    };
-    movies.push(movie);
-    res.send(movie);
-});
-
-// -------------------------------------------------- PUT --------------------------------------------------
-app.put("/api/movies/:id", (req, res) => {
-    const movie = movies.find((c) => c.id === parseInt(req.params.id));
-    if (!movie) return res.status(404).send("The movie with the given ID was not found.");
-
-    const { error } = validateMovie(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    movie.name = req.body.name;
-    res.send(movie);
-});
-
-// -------------------------------------------------- DELETE --------------------------------------------------
-app.delete("/api/movies/:id", (req, res) => {
-    const movie = movies.find((c) => c.id === parseInt(req.params.id));
-    if (!movie) return res.status(404).send("The movie with the given ID was not found.");
-
-    const index = movies.indexOf(movie);
-    movies.splice(index, 1);
-    res.send(movie);
-});
-
-
-
-// -------------------------------------------------- VALIDATE --------------------------------------------------
-function validateMovie(movie) {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required(),
-    });
-    return schema.validate(movie);
-}
-
+app.use('/api/genres', genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
